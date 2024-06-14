8fcc592d7474441c240000e02be772db11510f70
Simon Haines <nimon@hotmail.co.uk>
2024-06-14 12:07:40 +0100

    fixes example in docstring

--- arrays/21.js
+++ arrays/21.js
@@ -4,7 +4,7 @@
  * @param {number} newPrice - The new product price to add.
  * @returns {number[]} The new array of modified prices.
  * @example
- * updatePrices([30, 120, 50, 200], 45); // returns [60, 90]
+ * updatePrices([30, 120, 50, 200], 45); // returns [60, 100, 90]
  */
 export function updatePrices(prices, newPrice) {
   // TODO: Use the push() method to add the new price to the array
